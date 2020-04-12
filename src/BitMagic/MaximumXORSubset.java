d0c195b83069447031451659f229f1690697ea11
p.shakti <p.shakti@pshakti-PC>
2020-04-12 20:46:43 +0530

    feature | shaktipravesh | MaximumXORSubset Naive method
    Given a set A[ ] of positive integers. The task is to find the maximum
    XOR value of the elements from all the possible subsets.
    
    Input:
    The first line of input contains an integer T denoting the number of
    test cases. Then T test cases follow.
    The first line of each test case contains an integer N, where N is the
    size of the set A[ ]. The second line of each test case contains N space
    separated integers, denoting elements of the set A[ ].
    
    Output
    Print out the maximum XOR subset value.
    
    Constraints
    1 <= T <= 100
    1 <= N <= 30
    0 <= A[i] < 1000
    
    Examples:
    Input:
    3
    3
    2 4 5
    3
    33 73 64
    9
    5 633 64 43 131 51 87 999 23
    
    Output:
    7
    104
    1023

--- src/BitMagic/MaximumXORSubset.java
+++ src/BitMagic/MaximumXORSubset.java
@@ -9,9 +9,38 @@ public class MaximumXORSubset {
 		int iTestCt = 0;
 		iTestCt = sc.nextInt();
 		for(int iTemp = 1; iTemp <= iTestCt; iTemp++){
-			
+			int iSize = sc.nextInt();
+			int iMaxXOR = 0, iXORArraySize = iSize;
+			int[] iNumArray = new int[iSize];
+			int[] iXORArray = new int[iSize];
+			for(int i =0; i < iSize; i++) {
+				iNumArray[i] = sc.nextInt();
+				iXORArray[i] = iNumArray[i];
+				if(iMaxXOR < iNumArray[i])
+					iMaxXOR = iNumArray[i];
+			}
+			iMaxXOR = getMaxXOR(iNumArray, iXORArray, iSize, iXORArraySize-1, iMaxXOR);
+			System.out.println(iMaxXOR);
 		}
 
+	}
+
+	private static int getMaxXOR(int[] iNumArray, int[] iXORArray, int iSize, int iXORArraySize, int iMaxXOR) {
+		if(iXORArraySize < 1) {
+			return iMaxXOR;
+		}
+
+		for(int i = 0; i < iXORArraySize; i++) {
+			int[] iTempArray = new int[iXORArraySize-i];
+			for(int j = i, k = iSize - iXORArraySize + i, n = 0; j < iXORArraySize; j++, k++, n++) {
+				iTempArray[n] = iNumArray[k]^iXORArray[i];
+				if(iMaxXOR < iTempArray[n]) {
+					iMaxXOR = iTempArray[n];
+				}
+			}
+			iMaxXOR = getMaxXOR(iNumArray, iTempArray, iSize, iXORArraySize-i-1, iMaxXOR);
+		}
+		return iMaxXOR;
 	}
 
 }
