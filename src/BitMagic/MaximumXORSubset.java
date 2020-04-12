50f59118b04dde3383fe981655b981a8f601825d
p.shakti <p.shakti@pshakti-PC>
2020-04-12 13:05:51 +0530

    feature | p.shakti | 		Maximum XOR SubSet

---
+++ src/BitMagic/MaximumXORSubset.java
@@ -0,0 +1,17 @@
+package BitMagic;
+
+import java.util.Scanner;
+
+public class MaximumXORSubset {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int iTestCt = 0;
+		iTestCt = sc.nextInt();
+		for(int iTemp = 1; iTemp <= iTestCt; iTemp++){
+			
+		}
+
+	}
+
+}
