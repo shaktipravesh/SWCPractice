c2e12e6801bd44ed16b5f716eae78bf39210366b
p.shakti <p.shakti@pshakti-PC>
2020-04-15 19:50:29 +0530

    feature | shaktipravesh |

---
+++ geeksForGeeks/Arrays/PrintPattern.java
@@ -0,0 +1,55 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class PrintPattern {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int iTestCt = 0;
+		iTestCt = sc.nextInt();
+		for(int iTemp = 1; iTemp <= iTestCt; iTemp++){
+			int iNum = sc.nextInt();
+			printPattern(iNum, iNum);
+			System.out.println();
+		}
+		
+		sc.close();
+
+	}
+
+	private static void printPattern(int i, int iNum) {
+		System.out.print(i + " ");
+		if(((iNum >= 0) && (i <= 0)) || ((iNum <= 0) && (i >= 0)) || i == 0) {
+			return;
+		}
+		if(iNum > 0)
+			printPattern(i - 5, iNum);
+		if(iNum < 0)
+			printPattern(i + 5, iNum);		
+		
+		System.out.print(i + " ");
+	}
+
+}
+
+
+/*
+ * https://practice.geeksforgeeks.org/problems/print-pattern/0/ 
+ * Print Pattern
+ * Print a sequence of numbers starting with N, without using loop, in which
+ * A[i+1] = A[i] - 5, if A[i]>0, else A[i+1]=A[i] + 5 repeat it until A[i]=N.
+ * 
+ * Input: The first line contains an integer T, number of test cases. Then
+ * following T lines contains an integer N.
+ * 
+ * Output: For each test case, print the pattern in newline with space separated
+ * integers.
+ * 
+ * Constraints: 0< N < 10000
+ * 
+ * Example: Input: 2 16 10 Output: 16 11 6 1 -4 1 6 11 16 10 5 0 5 10
+ * 
+ * Explanation: We basically first reduce 5 one by one until we reach a negative
+ * or 0. After we reach 0 or negative, we one by one add 5 until we reach N.
+ */
