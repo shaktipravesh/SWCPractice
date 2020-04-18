27f6b380d0d82be03a6c542740cfe1a8cf185baf
p.shakti <p.shakti@pshakti-PC>
2020-04-18 11:40:06 +0530

    feature | shaktipravesh | heck-if-array-contains-  * contiguous-integers-with-duplicates-allowed

---
+++ geeksForGeeks/arrays/CheckIfArrayContainsContiguousIntegersWithDuplicatesAllowed.java
@@ -0,0 +1,69 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class CheckIfArrayContainsContiguousIntegersWithDuplicatesAllowed {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int iTestCt = 0;
+		iTestCt = sc.nextInt();
+		for(int iTemp = 1; iTemp <= iTestCt; iTemp++){
+			int iSize = sc.nextInt();
+			int iStart = 100000, iEnd = 0, iTrueCt = 0;
+			int[] iNumArray = new int[100001];
+			for(int i = 0; i < iSize; i++) {
+				int iNum = sc.nextInt();
+				if(iStart > iNum)
+					iStart = iNum;
+				if(iEnd < iNum)
+					iEnd = iNum;
+				if(iNumArray[iNum] == 0) {
+					iNumArray[iNum] = 1;
+					iTrueCt++;
+				}
+			}
+			if((iEnd - iStart + 1) == iTrueCt) {
+				System.out.println("Yes");
+			} else {
+				System.out.println("No");
+			}
+		}
+		sc.close();
+	}
+
+}
+
+
+
+/*
+ * https://practice.geeksforgeeks.org/problems/check-if-array-contains-
+ * contiguous-integers-with-duplicates-allowed/0/ Given an array of n
+ * integers(duplicates allowed). Print “Yes” if it is a set of contiguous
+ * integers else print “No”.
+ * 
+ * 
+ * INPUT: The first line consists of an integer T i.e. the number of test cases.
+ * First line of each test case consists of an integer n, denoting the size of
+ * array. Next line consists of n spaced integers, denoting elements of array.
+ * 
+ * 
+ * OUTPUT: Print “Yes” if it is a set of contiguous integers else print “No”.
+ * 
+ * 
+ * 
+ * CONSTRAINTS: 1<=T<=100 1<=n<100000 a[i]<=105
+ * 
+ * Example:
+ * 
+ * 2 8 5 2 3 6 4 4 6 6 7 10 14 10 12 12 13 15
+ * 
+ * Output : Yes No
+ * 
+ * Explanation: Test Case 1 : The elements of array form a contiguous set of
+ * integers which is {2, 3, 4, 5, 6} so the output is Yes. Test Case 2: We are
+ * unable to form contiguous set of integers using elements of array.
+ * 
+ */
+
+
