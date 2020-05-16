e6a95f47bb14f645db63c46bc0ff49e706629d8a
p.shakti <p.shakti@pshakti-PC>
2020-05-16 17:43:46 +0530

    DesignPattern | shaktipravesh | adapterdesign pattern

---
+++ designPattern/adapterDesign/AdapterDesignPattern.java
@@ -0,0 +1,10 @@
+package adapterDesign;
+
+public class AdapterDesignPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ designPattern/adapterDesign/AssignmentWork.java
@@ -0,0 +1,18 @@
+package adapterDesign;
+
+public class AssignmentWork {
+
+	private Pen p;
+	
+	public void writeAssignment(String str) {
+		
+	}
+
+	public Pen getP() {
+		return p;
+	}
+
+	public void setP(Pen p) {
+		this.p = p;
+	}
+}
